72496e9e26d0d5013d38d08eadcee4e6684bbc27
Erwin <xfsy_2015@163.com>
2021-03-08 21:25:51 +0800

    feat: mybatis-plus config

---
+++ src/main/java/com/fengwenyi/mybatisplusexample/config/MyBatisPlusConfiguration.java
@@ -0,0 +1,25 @@
+package com.fengwenyi.mybatisplusexample.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Erwin Feng
+ * @since 2021-03-08
+ */
+@Configuration
+@MapperScan("com.fengwenyi.mybatisplusexample.mapper")
+public class MyBatisPlusConfiguration {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+
+}
